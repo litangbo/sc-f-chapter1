cc7876c33b6407fee1d1d3ffad3ca5bfc18bbd0e
litangbo <384158865@qq.com>
2018-10-10 11:15:54 +0800

    9 服务链路追踪(Spring Cloud Sleuth)-引入zipkin依赖

---
+++ service-hi2/src/main/java/com/jc/servicehi2/ServiceHi2Application.java
@@ -0,0 +1,52 @@
+package com.jc.servicehi2;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ServiceHi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHi2Application.class, args);
+    }
+
+    //** 服务链路追踪(Spring Cloud Sleuth)——zipkin **//
+
+    private static final Logger LOG = Logger.getLogger(ServiceHi2Application.class.getName());
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+        return "i'm service-hi";
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+}
